d923141f912c751a1bad31e2c24e79917c2be963
IgorGewehr <gewehr.kt@gmail.com>
2024-11-14 11:40:33 -0300

    Projeto pronto

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-
 from router import router
-
+import uvicorn
 app = FastAPI()
-
 app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
